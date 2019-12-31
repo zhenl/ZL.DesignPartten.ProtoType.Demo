b938b54fcf3624965038a667b3a53fe89409fc1f
甄镭 <zhenlei1970@hotmail.com>
2019-12-31 15:59:17 +0800

    添加项目文件。

---
+++ ZL.DesignPartten.ProtoType.Demo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ZL.DesignPartten.ProtoType.Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
