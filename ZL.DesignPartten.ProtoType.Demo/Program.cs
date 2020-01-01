6f647877ab70f23fe507c832ea8447aeefe3768f
甄镭 <zhenlei1970@hotmail.com>
2020-01-01 09:34:46 +0800

    improve output

--- ZL.DesignPartten.ProtoType.Demo/Program.cs
+++ ZL.DesignPartten.ProtoType.Demo/Program.cs
@@ -47,32 +47,21 @@ namespace ZL.DesignPartten.ProtoType.Demo
 
             workflow.Links.Add(linkStartToInput);
             workflow.Links.Add(linkinputNodeToEnd);
-            Console.WriteLine("输出第一个节点的名称");
-            Console.WriteLine(workflow.Nodes[0].Name);
-            Console.WriteLine("通过连接引用修改开始节点的名称");
-            workflow.Links[0].Source.Name = "开始";
-            Console.WriteLine("应该修改为开始");
-            Console.WriteLine(workflow.Nodes[0].Name);
+
+            Console.WriteLine("比较节点集合中的对象与连接中引用的对象，看是否相同");
+            Console.WriteLine(workflow.Nodes[0] == workflow.Links[0].Source);
             Console.WriteLine("");
             var newworkflow = workflow.Clone_v0();
-            Console.WriteLine("输出克隆对象的第一个节点的名称");
-            Console.WriteLine(newworkflow.Nodes[0].Name);
-            Console.WriteLine("通过连接引用修改开始节点的名称");
-            newworkflow.Links[0].Source.Name = "开始1";
-            Console.WriteLine("名称没有被修改，说明Clone时Link中的每个节点创建了新的对象");
-            Console.WriteLine(newworkflow.Links[0].Source.Name);
-            Console.WriteLine(newworkflow.Nodes[0].Name);
+            Console.WriteLine("采用序列化的方法克隆对象，不进行引用处理");
+            Console.WriteLine("比较节点集合中的对象与连接中引用的对象，看是否相同");
+            Console.WriteLine(newworkflow.Nodes[0]== newworkflow.Links[0].Source);
+            
 
             Console.WriteLine("");
             var newworkflow1 = workflow.Clone();
-            Console.WriteLine("输出克隆对象的第一个节点的名称");
-            Console.WriteLine(newworkflow1.Nodes[0].Name);
-            Console.WriteLine("通过连接引用修改开始节点的名称");
-            newworkflow1.Links[0].Source.Name = "Start";
-            Console.WriteLine("名称相同,说明指向相同的节点");
-            Console.WriteLine(newworkflow1.Links[0].Source.Name);
-            Console.WriteLine(newworkflow1.Nodes[0].Name);
-
+            Console.WriteLine("采用序列化的方法克隆对象，对引用进行处理");
+            Console.WriteLine("比较节点集合中的对象与连接中引用的对象，看是否相同");
+            Console.WriteLine(newworkflow1.Nodes[0]== newworkflow1.Links[0].Source);
             Console.ReadLine();
         }
     }
